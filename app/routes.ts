44565f5f63ab226dbddb4b58e9ef90fca4020c6d
Nyan Prakash <nyanprakash@mac.mynetworksettings.com>
2025-10-03 14:53:13 -0400

    asdfasdfadfg

--- app/routes.ts
+++ app/routes.ts
@@ -3,7 +3,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 
 export default [
-  index("routes/home.tsx"),
+  index("routes/dashboard.tsx"),
   route("/dashboard", "routes/dashboard.tsx"),
   route("api/auth/*", "routes/api.auth.$.ts"),
   route("api/chat", "routes/api.chat.ts"),
