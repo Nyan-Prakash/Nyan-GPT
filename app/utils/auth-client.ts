8c2375a078adacdfdb31b9a70689c5f78db010d3
Nyan Prakash <nyanprakash@Nyans-MacBook-Air.local>
2025-10-03 13:56:11 -0400

    fixing for deployment

--- app/utils/auth-client.ts
+++ app/utils/auth-client.ts
@@ -2,6 +2,6 @@ import { createAuthClient } from 'better-auth/react'
 
 export const authClient = createAuthClient({
   // same-origin in dev, so baseURL can be omitted
-  baseURL: 'http://localhost:5173',
+  baseURL: 'https://*.vercel.app',
 })
 export const { signUp, signIn, signOut, useSession, getSession } = authClient
