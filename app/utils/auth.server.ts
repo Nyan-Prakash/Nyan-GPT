ce401489d189e33910c866e6084db34f1fbfea7d
Nyan Prakash <nyanprakash@Nyans-MacBook-Air.local>
2025-10-03 15:05:25 -0400

    asdfasdfadfg

--- app/utils/auth.server.ts
+++ app/utils/auth.server.ts
@@ -13,7 +13,7 @@ export const auth = betterAuth({
     usePlural: true,    
   }),
   emailAndPassword: { enabled: true },
-  trustedOrigins: ["https://whoisyou-sable.vercel.app"],
+  trustedOrigins: ["ttps://whoisyou-git-whoisyou-nyan-prakashs-projects.vercel.app"],
   cookies: {
     sessionToken: { name: "better-auth.session", sameSite: "lax", secure: false },
   },
