dcbec18e64f679be3e1bd50f428159390f83adaf
Nyan Prakash <nyanprakash@Nyans-MacBook-Air.local>
2025-10-03 13:36:16 -0400

    fixing for deployment

--- app/utils/auth.server.ts
+++ app/utils/auth.server.ts
@@ -13,7 +13,7 @@ export const auth = betterAuth({
     usePlural: true,    
   }),
   emailAndPassword: { enabled: true },
-  trustedOrigins: ["*.vercel.app"],
+  trustedOrigins: ["https://whoisyou-sable.vercel.app/"],
   cookies: {
     sessionToken: { name: "better-auth.session", sameSite: "lax", secure: false },
   },
