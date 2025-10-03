0020f7a5a3e25c86c1abe7e775fd9aba66f91a79
Nyan Prakash <nyanprakash@mac.mynetworksettings.com>
2025-10-03 15:33:32 -0400

    fuck

--- app/utils/auth-client.ts
+++ app/utils/auth-client.ts
@@ -1,5 +1,6 @@
 import { createAuthClient } from 'better-auth/react'
 
 export const authClient = createAuthClient({
+  baseURL: "https://whoisyou-sable.vercel.app/"
 })
 export const { signUp, signIn, signOut, useSession, getSession } = authClient
